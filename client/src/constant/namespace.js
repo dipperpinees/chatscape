cc940c34fffc02ddf0f302333986e1066aa17caa
Hiep Nguyen <79491170+hiepnguyen223@users.noreply.github.com>
2022-03-12 16:50:33 +0700

    Update namespace.js

--- client/src/constant/namespace.js
+++ client/src/constant/namespace.js
@@ -1,4 +1,2 @@
-export const API = "http://192.168.29.105:8000";
-export const SOCKET_API = "http://192.168.29.105:8000";
-// export const API = "https://chatappexpress.herokuapp.com"
-// export const SOCKET_API = "https://chat-appsocket.herokuapp.com/";
+export const API = process.env.REACT_APP_ENDPOINT;
+export const SOCKET_API = process.env.REACT_APP_ENDPOINT;
